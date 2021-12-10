d45201a95f1f5899b091da2cb444b895c73e2db4
hayoung0Lee <hayoung0.lee@gmail.com>
2021-12-10 17:37:08 +0900

    높이까지는 구해짐

--- src/components/Editor/Box.tsx
+++ src/components/Editor/Box.tsx
@@ -1,4 +1,10 @@
-import { useState, useEffect, useCallback } from "react";
+import {
+  useState,
+  useEffect,
+  useCallback,
+  useRef,
+  useLayoutEffect,
+} from "react";
 import yoga, { Node } from "yoga-layout-prebuilt";
 import JsBarcode from "jsbarcode";
 
@@ -7,20 +13,33 @@ interface PropType {
   computedLayout: any;
 }
 
-function convertToPx(pt: any): any {
-  if (!pt) {
+function handleSize(size: any, zoom: any = 1): any {
+  if (!size) {
     return "auto";
   }
 
-  if (pt.charAt(pt.length - 1) === "%") {
-    return pt;
+  if (size.charAt(size.length - 1) === "%") {
+    // percent인경우
+    return size;
   }
 
-  return `${parseInt(pt) * (96 / 72)}`;
+  // return `${parseInt(size) * (96 / 72)}`;
+  return `${parseInt(size) * zoom}`;
+}
+
+function setterName(key) {
+  return `set${key[0].toUpperCase()}${key.substr(1)}`;
 }
 
 const Box = ({ layoutDefinition, computedLayout }: PropType) => {
   const [currentLayout, setCurrentLayout] = useState<any>();
+  const [calcHeight, setCalcHeight] = useState<any>();
+
+  // text
+  const textRef = useCallback((node) => {
+    console.log("node", node.clientHeight);
+    setCalcHeight(node.clientHeight);
+  }, []);
 
   // barcode 생길때만
   const barcodeRef = useCallback((node) => {
@@ -28,7 +47,7 @@ const Box = ({ layoutDefinition, computedLayout }: PropType) => {
       JsBarcode(node, "12345", {
         format: "code128",
         width: 0.5, // 사이 간격을 말하는 듯
-        height: 20, // 높이 동일하게 주도록 계산해야할듯.
+        height: 20, // 이게 꽉채우는 방식으로 구현하기
         textMargin: 0,
         margin: 0,
         displayValue: false,
@@ -36,35 +55,27 @@ const Box = ({ layoutDefinition, computedLayout }: PropType) => {
     }
   }, []);
 
-  function setterName(key) {
-    return `set${key[0].toUpperCase()}${key.substr(1)}`;
+  function setNodeSize(curNode: any, layoutDefinition: any) {
+    const { type, flex } = layoutDefinition;
+    // auto
+    // type별 크기 계산 로직 만들기
+    if (type === "Container") {
+      curNode[setterName("width")](handleSize(flex?.size?.width));
+      curNode[setterName("height")](handleSize(flex?.size?.height));
+    } else if (type === "Text") {
+      // 하위 div의 높이를 가지고 세팅한다. 과연 가능할것인가!?
+    }
   }
 
   function createYogaNodes(layoutDefinition) {
     const curNode = Node.create();
-    const { type, flex } = layoutDefinition;
+    const { flex } = layoutDefinition;
+    setNodeSize(curNode, layoutDefinition);
 
-    // auto
-    // type별 크기 계산 로직 만들기
-    if (type === "Container") {
-      curNode[setterName("width")](convertToPx(flex?.size?.width));
-      curNode[setterName("height")](convertToPx(flex?.size?.height));
-    } else if (type === "Text") {
-      curNode[setterName("width")]("auto");
-      curNode[setterName("height")](
-        `${parseInt(layoutDefinition.text.text_size) + 1}`
-      );
-    } else if (type === "Barcode") {
-      curNode[setterName("width")]("auto");
-      curNode[setterName("height")]("20");
-    } else if (type === "Qrcode") {
-      //qrcode
-    }
-
+    // size와 관련되지 않은 다른 속성들.
     if (flex?.flex_direction) {
       curNode[setterName("flexDirection")](flex.flex_direction);
     }
-
     if (flex?.flex_grow) {
       curNode[setterName("flexGrow")](flex.flex_grow);
     }
@@ -75,7 +86,7 @@ const Box = ({ layoutDefinition, computedLayout }: PropType) => {
         try {
           curNode[setterName(key)](
             yoga[`EDGE_${dir.toUpperCase()}`],
-            flex[key][dir]
+            handleSize(flex[key][dir]) // 값이 있다면 일단 px인걸로 나중에 처리하게
           );
         } catch (e) {}
       });
@@ -107,14 +118,14 @@ const Box = ({ layoutDefinition, computedLayout }: PropType) => {
   function calculateLayout(layoutDefinition) {
     const curYogaNode = createYogaNodes(layoutDefinition);
     curYogaNode.calculateLayout(
-      convertToPx(layoutDefinition.flex?.size?.width),
-      convertToPx(layoutDefinition.flex?.size?.height),
+      handleSize(layoutDefinition.flex?.size?.width),
+      handleSize(layoutDefinition.flex?.size?.height),
       layoutDefinition.flex?.flex_direction || yoga.DIRECTION_LTR // width, height는 꼭 있고 direction은 없으면 LTR
     );
     setCurrentLayout(getComputedLayout(curYogaNode));
   }
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     calculateLayout(layoutDefinition);
   }, [layoutDefinition]);
 
@@ -134,7 +145,7 @@ const Box = ({ layoutDefinition, computedLayout }: PropType) => {
         left,
         top,
         width,
-        height,
+        height: height === 0 ? calcHeight : height,
       }}
     >
       {layoutDefinition.type === "Barcode" && (
@@ -142,7 +153,9 @@ const Box = ({ layoutDefinition, computedLayout }: PropType) => {
           <svg ref={barcodeRef}></svg>
         </div>
       )}
-      {layoutDefinition?.text?.text && <div>{layoutDefinition.text.text}</div>}
+      {layoutDefinition?.text?.text && (
+        <div ref={textRef}>{layoutDefinition.text.text}</div>
+      )}
       {(children || []).map((child, index) => {
         return (
           <Box
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -2,12 +2,13 @@ import "./index.css";
 import Box from "./Box";
 import { barcode } from "../../sample";
 
+// 숫자는 다 px이다.
 const Editor = () => (
   <div className="Editor">
     <Box
       layoutDefinition={{
         type: "Container",
-        flex: { size: { width: "50", height: "40" } },
+        flex: { size: { width: "200", height: "200" } },
         children: [barcode],
       }}
       computedLayout={null}
