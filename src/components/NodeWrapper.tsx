9ac19c7a6a3c173d919e95f61bc5c61d4a0d703a
Hayoung Lee <hylee@bgpworks.com>
2021-12-21 12:08:31 +0900

    NodeWrapper 코드 정리

--- src/components/NodeWrapper.tsx
+++ src/components/NodeWrapper.tsx
@@ -4,14 +4,6 @@ import TextNode from "./TextNode";
 import ContainerNode from "./ContainerNode";
 import Draggable from "./Draggable";
 import useCalculateLayout from "../hooks/useCalculateLayout";
-interface PropType {
-  layoutDefinition: any;
-  computedLayout: any;
-  path: any; // 여기까지 오는 path
-  onUpdateSelectedPath: any;
-  selectedPath: any;
-  onDragBox: any;
-}
 
 const NodeWrapper = ({
   layoutDefinition,
@@ -20,10 +12,8 @@ const NodeWrapper = ({
   onUpdateSelectedPath,
   selectedPath,
   onDragBox,
-}: PropType) => {
-  // 그래도 얘가 더 빠름. react children들은 왜 re-render 하는건지 모르겠음
+}: any) => {
   const currentLayout = useCalculateLayout(layoutDefinition);
-  // 현재의 computedLayout
   const curComputedLayout = computedLayout || currentLayout; // props로 받은것(부모가 계산한 layout) | 없으면(root 같은 경우) 현재 자기 값.
   const { left, top, width, height, children } = curComputedLayout;
 
